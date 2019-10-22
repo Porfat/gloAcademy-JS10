bc435554d13c4e2a7f960200d675aecf3aa0f55c
Filip Solovjov <fil.solovjov@gmail.com>
2019-10-22 16:08:58 +0200

    Extra HW moved to 'bonus' folder

---
+++ bonus/script.js
@@ -0,0 +1,23 @@
+// Создать переменную num со значением 266219:
+
+let num = 266219;
+
+// Вывести в консоль произведение (умножение) цифр этого числа:
+
+let digits = num.toString().split('');
+
+product = num.toString().split('').reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+}, 1);
+
+console.log(`Произведение числа из num: ${product}`);
+
+// Полученный результат возвести в степень 3, используя только 1 оператор:
+
+productThirdPower = product**3
+
+console.log(`${product} ^ 3 = ${productThirdPower}`);
+
+// Вывести на экран первые 2 цифры полученного числа:
+
+console.log(`Первые две цифры из полученного числа: ${(productThirdPower).toString().substring(0,2)}`);
