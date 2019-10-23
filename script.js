3b72d165d440a358b317dc89e0683aee9100f879
Filip Solovjov <fil.solovjov@gmail.com>
2019-10-23 17:36:19 +0200

    Upload third homework

--- script.js
+++ script.js
@@ -1,14 +1,62 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+'use strict';
 
-<<<<<<< HEAD:script.js
-alert('Hello master branch!');
-=======
-alert('Hello new git branch!');
->>>>>>> lesson01:lesson01/script.js
+// Спрашиваем у пользователя “Ваш месячный доход?” и результат сохраняем в переменную money:
+let money = prompt('Ваш месячный доход?'),
 
-console.log('Message-101'); 
+// Спросить у пользователя “Перечислите возможные расходы за рассчитываемый период через запятую” сохранить в переменную addExpenses, вывести в консоль в виде массива:
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+
+// Спросить у пользователя “Есть ли у вас депозит в банке?”
+// сохранить данные в переменной deposit (булевое значение true/false):
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    mission = 100000,
+    period = 5;
+
+    console.log(addExpenses.split(','));
+
+// Вывести в консоль типы данных money, income, deposit:
+
+console.log(typeof(money));
+console.log(typeof(addExpenses));
+console.log(typeof(deposit));
+
+// Спросить у пользователя по 2 раза каждый вопрос и записать ответы в переменные:
+
+let mandatoryExp1 = prompt('1. Какие обязательные ежемесячные расходы у вас есть?'),
+mandatoryExp1Cost = prompt('1. Во сколько это обойдется?'),
+mandatoryExp2 = prompt('2. Какие обязательные ежемесячные расходы у вас есть?'),
+mandatoryExp2Cost = prompt('2. Во сколько это обойдется?'),
+
+// Вычислить доход за месяц, учитывая обязательные расходы, сохранить в переменную budgetMonth и вывести результат в консоль:
+
+budgetMonth = money-mandatoryExp1Cost-mandatoryExp2Cost;
+
+// Зная budgetMonth, посчитать за сколько месяцев будет достигнута цель mission, вывести в консоль, округляя в большую сторону:
+
+console.log(`Цель будет достигнута за: ${Math.ceil(mission/budgetMonth)} месяцев`);
+
+// Поправить budgetDay учитывая бюджет на месяц, а не месячный доход. Вывести в консоль  округлив в меньшую сторону:
+
+let budgetDay = budgetMonth / 30;
+console.log(`Бюджет на день: ${Math.floor(budgetDay)}`);
+
+
+switch (true) {
+    case (budgetDay <= 800):
+        console.log('Высокий уровень дохода');
+        break;
+
+    case (300 > budgetDay < 800):
+        console.log('Средний уровень дохода');
+        break;
+
+    case (0 > budgetDay < 300):
+        console.log('Низкий уровень дохода');
+        break;
+
+    default:
+        console.log('Что то пошло не так');
+        break;
+}
+
+
