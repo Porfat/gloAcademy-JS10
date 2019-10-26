eb7f6ab2b783b34d9fc0d81a028018a1508d9e04
Filip Solovjov <fil.solovjov@gmail.com>
2019-10-26 23:19:07 +0200

    Uploaded bonus HW4

--- bonus/script.js
+++ bonus/script.js
@@ -1,45 +1,25 @@
 'use strict';
 
-let days = [
-    ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'],
-    ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье']
-],
+let sentence = prompt('Write something in the field', '123');
 
-lang = prompt('Language? (en, ru)').toLowerCase();
+function test (a) {
+    
+    if (typeof a !== 'string'){
+    
+        console.log(`Entered value "${a}" isn't a string`);
+    
+    } else if (a.length > 30){
+            
+        console.log(`String w/o spaces: "${a.replace(/\s/g, '').substring(0,30)}..."`);
+        
+    } else {
+        
+        console.log(`String w/o spaces: "${a.replace(/\s/g, '')}"`);
 
-// Решите задачу через if:
+    }
 
-if (lang == 'en'){
-    console.log(days[0]);
-}
-
-else {
-    console.log(days[1]);
 };
 
-// Решите задачу через switch:
 
-switch (lang) {
-
-    case 'en':
-        console.log(days[0]);
-        break;
-
-    case 'ru':
-        console.log(days[1]);
-        break;
-};
-
-// Решите задачу через многомерный массив без ифов и switch:
-
-lang == 'en' ? console.log(days[0]) : console.log(days[1]);
-
-
-// Решить задачу (имя) с помощью нескольких тернарных операторов, без использования if или switch:
-
-let namePerson = prompt('Имя?').toLowerCase();
-
-namePerson == 'артем' ? console.log('Это директор') :
-namePerson == 'максим' ? console.log('Это преподователь'):
-console.log('Это студент');
+test(sentence);
 
