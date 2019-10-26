8fd2f500167187a58e0c379f5623d401c5183498
Filip Solovjov <fil.solovjov@gmail.com>
2019-10-26 23:46:02 +0200

    Fixed (finally) conditions for switch

--- script.js
+++ script.js
@@ -38,11 +38,11 @@ switch (true) {
         console.log('Высокий уровень дохода');
         break;
 
-    case (300 < budgetDay <= 800):
+    case (budgetDay < 800 && budgetDay > 300 ):
         console.log('Средний уровень дохода');
         break;
 
-    case (0 < budgetDay <= 300):
+    case (budgetDay <= 300 && budgetDay >= 0):
         console.log('Низкий уровень дохода');
         break;
 
